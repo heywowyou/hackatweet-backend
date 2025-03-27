344d1014dfc3e61ae216b8c88564bcee41e04e21
heywowyou <pro@julesfontaine.com>
2025-03-27 10:50:08 +0100

    Edit app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const pingRouter = require("./routes/ping");
 
 var app = express();
 
@@ -20,8 +21,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-const pingRouter = require("./routes/ping");
 app.use("/", pingRouter);
 
 module.exports = app;
