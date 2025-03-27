19b7bf6aef2db3898bd2077c4ffce9552150a64e
heywowyou <pro@julesfontaine.com>
2025-03-27 15:03:08 +0100

    Add avatar to users schema

--- models/users.js
+++ models/users.js
@@ -6,6 +6,11 @@ const userSchema = mongoose.Schema({
   password: String,
   token: String,
   createdAt: Date,
+  avatar: {
+    type: String,
+    default:
+      "https://us.123rf.com/450wm/yuliarudenko/yuliarudenko1904/yuliarudenko190400001/122602696-oeuf-d-animal-r%C3%A9aliste-unique-blanc-de-vecteur-oeuf-de-poulet-isol%C3%A9-avec-des-ombres-douces-sur-fond.jpg?ver=6",
+  },
 });
 
 const User = mongoose.model("users", userSchema);
