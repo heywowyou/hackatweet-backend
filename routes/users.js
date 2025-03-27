830eb330c526f7131e70cdba631670f053e32b64
heywowyou <pro@julesfontaine.com>
2025-03-27 11:07:46 +0100

    Fix users route name s

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const User = require("../models/user");
+const User = require("../models/users");
 const bcrypt = require("bcrypt");
 const uid2 = require("uid2");
 
